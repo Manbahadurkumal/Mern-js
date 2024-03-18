f3a0c8cb59dff14c9771e07c131b58861920170b
Manbahadurkumal <mk9307888@gmail.com>
2024-03-18 21:44:40 +0545

    promise

--- Promise.js
+++ Promise.js
@@ -1,5 +1,22 @@
 const login = (username, password) =>{
-    return new Promise((resolve, reject) =>[
-        
-    ])
+    return new Promise((resolve, reject) =>{
+        const loginSuccess = true;
+        if(loginSuccess){
+            resolve({name: "manish"})
+        }else{
+            reject("Error")
+        }
+    })
 }
+login("admin", "admin123")
+.then(()=>{})
+.catch()
+console.log("I am last Line")
+// const alwaysResolve = () =>{
+//     return new Promise((res, rej) =>{
+//         res("I am resolve")
+//     })
+// }
+// alwaysResolve()
+// .then(res)
+// console.log(alwaysResolve)
