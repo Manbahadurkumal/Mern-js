0d804dfb9f8fef27afa7aedcaf5860cfbe8c4374
Manbahadurkumal <mk9307888@gmail.com>
2024-03-19 17:08:02 +0545

    inheritance

--- inheritance.js
+++ inheritance.js
@@ -7,3 +7,15 @@ class User{
     #password;
 }
 // user class will be inherited(parent class) to Admin (derived/ child class)
+// all the public properties of User can be access by Admin class ( using it's object)
+class Admin extends User{
+    role = 'admin';
+
+}
+class Student extends User {
+    role = "student"
+}
+const admin = new Admin()
+admin.name = "Manish"
+admin.email = "mk9307888@gmail.com"
+console.log(admin)
