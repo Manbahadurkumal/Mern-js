2b0ca85d1cffbb2877c1534d9b23b884510bc157
Manbahadurkumal <mk9307888@gmail.com>
2024-03-20 11:52:20 +0545

    es-standard

--- es-standard.js
+++ es-standard.js
@@ -2,13 +2,23 @@
 // commonjs, modulejs
 // .js, .jsx, .cjs, .mjs, ejs
 // ECMA Script
-
+// 
 // import export 
-
-const test = ()=>{
-
-} 
+ 
 
 //export es5
+//  module.exports = {
+//     test
+//  }
 
+
+// export es6
+export const test = ()=>{
+
+}
+export const testValue = ""
+export class User{
+
+}
+export default Main
 
---
+++ es-use.js
@@ -0,0 +1,14 @@
+// es 5
+// const standard = require("./es-standard");
+// const {test} = require("./es-standard")
+// const fs = require('fs')
+//requestAnimationFrame("./array")
+
+// named export =====> import
+import User, {test} from "./es-standard"
+import {test as testFunction} from "./function"
+// import "./array"
+import * as AliasName from "./asyn-await"
+ import * as Yup from "yup"
+
+ 
